5c01cb5d4f1151dd1820019bb2d63d043d6741b2
awajiya-n <142461917+awajiya-n@users.noreply.github.com>
2023-09-20 14:19:23 +0900

    Delete MinnadeOsoziChan directory

--- MinnadeOsoziChan/src/dao/DbConnection.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-public interface DbConnection {
-	public final String URL = "jdbc:mariadb://localhost:3307/clean";
-	public final String USER = "root";
-	public final String PASS = "root";
-
-}
